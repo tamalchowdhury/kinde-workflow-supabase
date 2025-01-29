98ecde2a7be5439b25754887494cb1206f7f82be
Peter Phanouvong <peterphanouvong@gmail.com>
2025-01-29 11:36:19 +1100

    new workflow

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -22,22 +22,18 @@ export default async function (event: onUserTokenGeneratedEvent) {
     data: any;
   }>();
 
-  try {
-    const response = await fetch(
-      "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles",
-      {
-        headers: {
-          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    accessToken.data = data;
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  const response = await fetch(
+    "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles",
+    {
+      headers: {
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+  accessToken.data = data;
 
   accessToken.hello = "Hello there!";
   accessToken.ipAddress = event.request.ip;
