4838c84b51e573cf9dcaa0e78de2806df1b5f5e8
Peter Phanouvong <peterphanouvong@gmail.com>
2025-01-29 11:35:06 +1100

    try new workflow

--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -11,6 +11,7 @@ export const workflowSettings: WorkflowSettings = {
   bindings: {
     "kinde.accessToken": {},
     "kinde.localization": {},
+    "kinde.fetch": {},
   },
 };
 
@@ -18,7 +19,25 @@ export default async function (event: onUserTokenGeneratedEvent) {
   const accessToken = accessTokenCustomClaims<{
     hello: string;
     ipAddress: string;
+    data: any;
   }>();
+
+  try {
+    const response = await fetch(
+      "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles",
+      {
+        headers: {
+          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    accessToken.data = data;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 
   accessToken.hello = "Hello there!";
   accessToken.ipAddress = event.request.ip;
