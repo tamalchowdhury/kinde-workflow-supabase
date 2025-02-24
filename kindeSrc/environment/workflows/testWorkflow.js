6cdab45f146bc0cc0f2eb8d87603bdd3d5b775ed
Peter Phanouvong <peterphanouvong@gmail.com>
2025-02-24 12:06:01 +1100

    test workflow

---
+++ kindeSrc/environment/workflows/testWorkflow.js
@@ -0,0 +1,75 @@
+var a = /* @__PURE__ */ ((e) => (
+  (e.UserTokenGeneration = "user:tokens_generation"),
+  (e.UserPreMFA = "user:pre_mfa"),
+  (e.M2MTokenGeneration = "m2m:token_generation"),
+  (e.ExistingPasswordProvided = "user:existing_password_provided"),
+  (e.NewPasswordProvided = "user:new_password_provided"),
+  e
+))(a || {});
+const r = {
+  get(e, n, t) {
+    return Reflect.get(e, n.toString(), t);
+  },
+  set(e, n, t) {
+    return kinde.accessToken.setCustomClaim(n, t), Reflect.set(e, n, t);
+  },
+};
+function d() {
+  if (!kinde.accessToken)
+    throw new Error(
+      "accessToken binding not available, please add to workflow/page settings to enable"
+    );
+  const e = kinde.accessToken.getCustomClaims();
+  return new Proxy(e, r);
+}
+function c(e) {
+  if (!kinde.env)
+    throw new Error(
+      "env binding not available, please add to workflow/page settings to enable"
+    );
+  return kinde.env.get(e);
+}
+async function k(e, n) {
+  if (!kinde.fetch) throw new Error("fetch binding not available");
+  n.responseFormat || (n.responseFormat = "json");
+  const t = await kinde.fetch(e, n);
+  return {
+    data: n.responseFormat === "json" ? (t == null ? void 0 : t.json) : t.text,
+  };
+}
+const l = {
+  id: "addAccessTokenClaim",
+  trigger: a.UserTokenGeneration,
+  bindings: {
+    "kinde.accessToken": {},
+    "kinde.localization": {},
+    "kinde.fetch": {},
+    "kinde.env": {},
+    "kinde.mfa": {},
+    url: {},
+  },
+};
+async function u(e) {
+  var i;
+  const n = (i = c("SUPABASE_ANON_KEY")) == null ? void 0 : i.value,
+    t = d(),
+    s = await k(
+      "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles?kinde_id=eq." +
+        e.context.user.id,
+      {
+        method: "GET",
+        headers: {
+          apikey: n,
+          Authorization: `Bearer ${n}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  if (s.data.length > 0) {
+    const o = s.data[0];
+    t.isSubscribed =
+      (o == null ? void 0 : o.is_on_monthly_subscription) ||
+      (o == null ? void 0 : o.paid_one_time_subscription);
+  }
+}
+export { u as default, l as workflowSettings };
--- kindeSrc/environment/workflows/testWorkflow.ts
+++ kindeSrc/environment/workflows/testWorkflow.ts
@@ -1,47 +1,47 @@
-import {
-  onUserTokenGeneratedEvent,
-  accessTokenCustomClaims,
-  WorkflowSettings,
-  WorkflowTrigger,
-  fetch,
-  getEnvironmentVariable,
-} from "@kinde/infrastructure";
+// import {
+//   onUserTokenGeneratedEvent,
+//   accessTokenCustomClaims,
+//   WorkflowSettings,
+//   WorkflowTrigger,
+//   fetch,
+//   getEnvironmentVariable,
+// } from "@kinde/infrastructure";
 
-export const workflowSettings: WorkflowSettings = {
-  id: "addAccessTokenClaim",
-  trigger: WorkflowTrigger.UserTokenGeneration,
-  bindings: {
-    "kinde.accessToken": {},
-    "kinde.localization": {},
-    "kinde.fetch": {},
-    "kinde.env": {},
-    url: {},
-  },
-};
+// export const workflowSettings: WorkflowSettings = {
+//   id: "addAccessTokenClaim",
+//   trigger: WorkflowTrigger.UserTokenGeneration,
+//   bindings: {
+//     "kinde.accessToken": {},
+//     "kinde.localization": {},
+//     "kinde.fetch": {},
+//     "kinde.env": {},
+//     url: {},
+//   },
+// };
 
-export default async function TestWorkflow(event: onUserTokenGeneratedEvent) {
-  const SUPABASE_ANON_KEY = getEnvironmentVariable("SUPABASE_ANON_KEY")?.value;
-  const accessToken = accessTokenCustomClaims<{
-    isSubscribed: boolean;
-  }>();
+// export default async function TestWorkflow(event: onUserTokenGeneratedEvent) {
+//   const SUPABASE_ANON_KEY = getEnvironmentVariable("SUPABASE_ANON_KEY")?.value;
+//   const accessToken = accessTokenCustomClaims<{
+//     isSubscribed: boolean;
+//   }>();
 
-  const response = await fetch(
-    "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles?kinde_id=eq." +
-      event.context.user.id,
-    {
-      method: "GET",
-      headers: {
-        apikey: SUPABASE_ANON_KEY,
-        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+//   const response = await fetch(
+//     "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles?kinde_id=eq." +
+//       event.context.user.id,
+//     {
+//       method: "GET",
+//       headers: {
+//         apikey: SUPABASE_ANON_KEY,
+//         Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+//         "Content-Type": "application/json",
+//       },
+//     }
+//   );
 
-  if (response.data.length > 0) {
-    const profile = response.data[0];
-    accessToken.isSubscribed =
-      profile?.is_on_monthly_subscription ||
-      profile?.paid_one_time_subscription;
-  }
-}
+//   if (response.data.length > 0) {
+//     const profile = response.data[0];
+//     accessToken.isSubscribed =
+//       profile?.is_on_monthly_subscription ||
+//       profile?.paid_one_time_subscription;
+//   }
+// }
