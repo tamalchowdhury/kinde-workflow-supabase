8dbeb23fb9eaa5e72685b8673bf043f859dc3f1c
Peter Phanouvong <peterphanouvong@gmail.com>
2025-02-25 14:34:05 +1100

    email workflow

---
+++ kindeSrc/environment/workflows/emailFilterWorkflow.ts
@@ -0,0 +1,52 @@
+import {
+  onUserTokenGeneratedEvent,
+  WorkflowSettings,
+  WorkflowTrigger,
+} from "@kinde/infrastructure";
+
+export const workflowSettings: WorkflowSettings = {
+  id: "postAUthentication",
+  name: "Post-authentication",
+  failurePolicy: {
+    action: "stop",
+  },
+  trigger: WorkflowTrigger.UserTokenGeneration,
+  bindings: {
+    "kinde.accessToken": {},
+    "kinde.localization": {},
+    "kinde.fetch": {},
+    "kinde.env": {},
+    "kinde.auth": {},
+    url: {},
+  },
+};
+
+export default async function EmailFilerWorkflow(
+  event: onUserTokenGeneratedEvent
+) {
+  console.log(event);
+  // const SUPABASE_ANON_KEY = getEnvironmentVariable("SUPABASE_ANON_KEY")?.value;
+  // const accessToken = accessTokenCustomClaims<{
+  //   isSubscribed: boolean;
+  // }>();
+
+  // const response = await fetch(
+  //   "https://xvyhaoxzkelgkbpeeqbp.supabase.co/rest/v1/profiles?kinde_id=eq." +
+  //     event.context.user.id,
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       apikey: SUPABASE_ANON_KEY,
+  //       Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+  //       "Content-Type": "application/json",
+  //     },
+  //   }
+  // );
+
+  // if (response.data.length > 0) {
+  //   const profile = response.data[0];
+  //   accessToken.isSubscribed =
+  //     profile?.is_on_monthly_subscription ||
+  //     profile?.paid_one_time_subscription;
+  // }
+}
